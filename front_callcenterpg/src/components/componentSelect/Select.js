8aa17d3ff527e2bc83dad54e0922645b0e535253
FelipeLeonardo98 <felipeferrazleonardo98@gmail.com>
2021-03-03 23:49:25 -0300

    Tests - Descriptions return

--- front_callcenterpg/src/components/componentSelect/Select.js
+++ front_callcenterpg/src/components/componentSelect/Select.js
@@ -2,10 +2,11 @@
 import React, { useEffect, useState } from 'react';
 import api from '../../service/api';
 import './Select.css';
-import Panel from '../componentPanel/Panel';
+//import Panel from '../componentPanel/Panel';
 
 export default function Select() {
     const [categories, setCategories] = useState(['']);
+    const [descriptions, setDescriptions] = useState(['']);
     // const response = {};
 
     useEffect(() => {
@@ -18,33 +19,40 @@ export default function Select() {
 
     }, []);
 
-    async function myFunction(e) {
+    async function MyFunction(e) {
         const search = e.target.value;
         try {
-            //const response = await api.post('/bycategory', { search: search });
-            // console.log(search);
-            <Panel parametro={search} />
-            // console.log(response.data);
+            const { data } = await api.post('/bycategory', { search: search });
+            // console.log(data);
+
+            data.map((datas, index) => {
+                return setDescriptions(datas.description);
+                //console.log(datas.description)
+            })
+
+
+
         } catch (error) {
             console.warn(error);
         }
 
     }
+
     return (
         <>
             <span>Selecione a situação</span>
-            <select onChange={myFunction}>
+            <select onChange={MyFunction}>
                 <option selected disabled> Categoria </option>
                 {categories.map((values, index) => {
                     return (
-
                         <option key={index} value={values}>{values}</option>
                     )
-
                 })}
-            </select>
-            <Panel />
-
+            </select> <br /> <hr />
+            <div>
+                {descriptions}
+            </div>
+            {console.log(descriptions)}
         </>
     )
 }
