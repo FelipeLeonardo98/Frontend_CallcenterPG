c733f5b9b2ee5e810a874ed15013b7d697d31dc8
FelipeLeonardo98 <felipeferrazleonardo98@gmail.com>
2021-03-17 21:20:18 -0300

    return into DOM - OK

--- front_callcenterpg/src/components/componentSelect/Select.js
+++ front_callcenterpg/src/components/componentSelect/Select.js
@@ -7,7 +7,7 @@ import './Select.css';
 export default function Select() {
     const [categories, setCategories] = useState(['']);
     const [descriptions, setDescriptions] = useState(['']);
-    var vetor = [];
+    // var vetor = [];
     // var vetorSplit;
     // const response = {};
 
@@ -24,50 +24,16 @@ export default function Select() {
     async function MyFunction(e) {
         const search = e.target.value;
         try {
-            const result = await api.post('/bycategory', { search: search });
-            const apoio = result.data;
-            //console.log(result.data);
-            //setDescriptions(data.description);
-            // console.log(`Teste: ${descriptions}`);
-            apoio.map((datas) => {
-                //     vetor.push(datas.description)
-                setDescriptions(datas.description);
-                return (console.log(`Teste: ${descriptions}`))
-            })
+            const { data } = await api.post('/bycategory', { search: search });
+            const descriptionMap = data.map(({ description }) => description)
+            // < Component args={descriptionMap} />
+            setDescriptions(descriptionMap);
 
         } catch (error) {
             console.warn(error);
         }
 
     }
-
-
-    /* async function MyFunction(e) {
-         const search = e.target.value;
-         try {
-             const { data } = await api.post('/bycategory', { search: search });
-             // console.log(data);
- 
- 
-             data.map((datas, index) => {
-                 //  return (console.log(`Index: ${index} - Descrição: ${datas.description}`))
-                 //   return (vetor.push(datas.description))
- 
-                 setDescriptions(datas.description);
-                 return (console.log(`Retorno do map: ${descriptions}`))
- 
-             })
- 
-             // console.log(`Valor do useState: ${descriptions}`)
-             //var vetorSplit = vetor.split(",");
-             console.log(`Retorno do vetor: ${descriptions}`);
-             //  console.log("outro retorno: " + vetorSplit);
- 
-         } catch (error) {
-             console.warn(error);
-         }
- 
-     }*/
 
     return (
         <>
@@ -81,7 +47,12 @@ export default function Select() {
                 })}
             </select> <br /> <hr />
             <ul>
+                {
+                    descriptions.map((data, id) => {
+                        return (<li key={id}>{data}</li>)
 
+                    })
+                }
             </ul>
 
         </>
